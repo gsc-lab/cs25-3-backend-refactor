b8b56ac3ecfa9636ff72fed86166c5c9e4cac60b
HARUNA <166793731+TANAKAHARUNA7@users.noreply.github.com>
2025-12-24 18:29:40 +0900

    chore: add PHPDoc blocks (#51)

--- App/Repository/TimeoffRepository.php
+++ App/Repository/TimeoffRepository.php
@@ -14,7 +14,10 @@ class TimeoffRepository {
     } 
 
 
-    // 디자이너 전체 휴무 출력
+    /**
+     *  디자이너 전체 휴무 출력
+     * @return array  휴무 정보 전체 출력
+     */
     public function index():array {
         
         // 휴무(TimeOff) 데이터를 사용자 정보와 함께 조회하고, 시작 시간 → 사용자 ID 순으로 정렬하는 쿼리
@@ -33,84 +36,83 @@ class TimeoffRepository {
     }
 
 
-    // designer 휴무 작성
+    /** 
+     * designer 휴무 작성
+     * @param int $userId designer의 user_id
+     * @return int 성공 1 / 실패 0
+     */ 
     public function create(
-        int $user_id,
-        string $start_at,
-        string $end_at 
+        int $userId,
+        string $startAt,
+        string $endAt 
     ):bool {
 
         $stmt = $this->db->prepare("INSERT INTO 
                                     TimeOff (user_id, start_at, end_at) 
                                     VALUES (?,?,?)");
-        $stmt->bind_param('iss', $user_id, $start_at, $end_at);
+        $stmt->bind_param('iss', $userId, $startAt, $endAt);
         $stmt->execute();
 
         return $stmt->affected_rows > 0;
     }
 
 
-    // designer 휴무 수정
+    /** 
+     * designer 휴무 수정
+     * @param int    $toId    휴무 ID
+     * @param string $startAt 휴무 시작 일시
+     * @param string $endAt   휴무 종료 일시
+     * @return int 성공 1 / 실패 0
+     */ 
+    
     public function update(
-        int $to_id,
-        string $start_at,
-        string $end_at
+        int $toId,
+        string $startAt,
+        string $endAt
     ):int {
 
-        // to_id가 DB에 있는지 check
-        $stmt = $this->db->prepare("SELECT 1 FROM TimeOff WHERE to_id = ? LIMIT 1");
-        $stmt->bind_param('i', $to_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        // 해당하지 않는 to_id가 넘어 왔을 때
-        if ($result->num_rows === 0) {
-            json_response([
-                'success' => false,
-                'error'   => [
-                    'code'    => 'TIMEOFF_NOT_FOUND',
-                    'message' => '해당 휴무 정보가 존재하지 않습니다.'
-                    ]
-            ], 400);
-        }
-
-        $stmt2 = $this->db->prepare("UPDATE TimeOff SET 
+        $stmt = $this->db->prepare("UPDATE TimeOff SET 
                                 start_at = ?, end_at = ?
                                 WHERE to_id=?");               
-        $stmt2->bind_param('ssi', $start_at, $end_at, $to_id);
-        $stmt2->execute();
+        $stmt->bind_param('ssi', $startAt, $endAt, $toId);
+        $stmt->execute();
 
-        return $stmt2->affected_rows;
+        return $stmt->affected_rows;
     }
 
 
-    // designer 휴무 삭제
+    /** 
+     * designer 휴무 삭제
+     * @param int    $toId    휴무 ID
+     * @return int 성공 1 / 실패 0
+     */ 
     public function delete(
-        int $to_id
+        int $toId
     ):int {
 
-        // to_id가 DB에 있는지 check
-        $stmt = $this->db->prepare("SELECT 1 FROM TimeOff WHERE to_id = ? LIMIT 1");
-        $stmt->bind_param('i', $to_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        // 해당하지 않는 to_id가 넘어 왔을 때
-        if ($result->num_rows === 0) {
-            json_response([
-                'success' => false,
-                'error'   => [
-                    'code'    => 'TIMEOFF_NOT_FOUND',
-                    'message' => '해당 휴무 정보가 존재하지 않습니다.'
-                    ]
-            ], 400);
-        }
-
-        $stmt2 = $this->db->prepare("DELETE FROM TimeOff WHERE to_id=?");
-        $stmt2->bind_param('i',$to_id);
+        $stmt = $this->db->prepare("DELETE FROM TimeOff WHERE to_id=?");
+        $stmt->bind_param('i',$toId);
             
-        $stmt2->execute();
+        $stmt->execute();
 
-        return $stmt2->affected_rows;
+        return $stmt->affected_rows;
+    }
+
+
+    /**
+     * 특정 휴무 정보를 조회한다.
+     *
+     * @param int $toId 휴무 ID
+     * @return int 조회 결과 (존재 여부 확인 )
+     */
+    public function show(
+        int $toId
+    ): int {
+
+        $stmt = $this->db->prepare("SELECT 1 FROM TimeOff WHERE to_id = ? LIMIT 1");
+        $stmt->bind_param('i', $toId);
+        $stmt->execute();
+
+        return $stmt->affected_rows;
     }
 }
